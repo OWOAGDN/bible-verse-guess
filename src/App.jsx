d801e7272b23553d35c8e4d7017d5a5db958e138
Jordan Dwaine <jdjb90@hotmail.com>
2025-07-11 15:36:04 +0100

    project start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Game from "../src/components/Game";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="text 3x1 font-bold">Guess the Verse!</h1>
+      </header>
+      <Game />
+    </>
+  );
+}
+
+export default App;
